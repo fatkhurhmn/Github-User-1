f2095b87319b8189c709cc1240d816823d850877
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-02-23 17:35:52 +0700

    menambahkan user model

---
+++ app/src/main/java/academy/bangkit/githubuser/model/UserModel.kt
@@ -0,0 +1,16 @@
+package academy.bangkit.githubuser.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserModel(
+    var name: String,
+    var username: String,
+    var location: String,
+    var repository: String,
+    var company: String,
+    var followers: String,
+    var following: String,
+    var avatar: String
+) : Parcelable
