8ab555ba52c6bafac7a8a4e92011aa891c6add92
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-02-23 18:39:50 +0700

    menampilkan recyler view

--- app/src/main/java/academy/bangkit/githubuser/MainActivity.kt
+++ app/src/main/java/academy/bangkit/githubuser/MainActivity.kt
@@ -1,11 +1,29 @@
 package academy.bangkit.githubuser
 
-import androidx.appcompat.app.AppCompatActivity
+import academy.bangkit.githubuser.adapter.UserAdapter
+import academy.bangkit.githubuser.databinding.ActivityMainBinding
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
+
+    private val userAdapter: UserAdapter by lazy { UserAdapter() }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        showListUsers()
+    }
+
+    private fun showListUsers() {
+        userAdapter.setListUsers(DummyData.getListUsers(this))
+        with(binding.rvUser) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = userAdapter
+        }
     }
 }
